723e82565ae10e6716316e8d5cec2b36bc7ce5cb
Олег Гуров <guroff.oleg@gmail.com>
2023-02-16 01:02:43 +0300

    Generic Views: высокоуровневые view-классы

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from cats.views import APICat, APICatDetail
+from cats.views import CatDetail, CatList
 
 urlpatterns = [
-    path('cats/', APICat.as_view()),
-    path('cats/<int:pk>/', APICatDetail.as_view()),
+    path('cats/', CatList.as_view()),
+    path('cats/<int:pk>/', CatDetail.as_view()),
 ]
