738aef0ac4311faa9a6f4de43fb28396451d0697
Олег Гуров <guroff.oleg@gmail.com>
2023-02-16 00:44:29 +0300

    Низкоуровневые view-классы в DRF

--- cats/views.py
+++ cats/views.py
@@ -1,35 +1,46 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Cat
 from .serializers import CatSerializer
 
 
-@api_view(['GET', 'POST'])
-def cat_list(request):
-    if request.method == 'POST':
-        serializer = CatSerializer(data=request.data, many=True)
+class APICat(APIView):
+    def get(self, request):
+        cats = Cat.objects.all()
+        serializer = CatSerializer(cats, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = CatSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    cats = Cat.objects.all()
-    serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
-def cat_detail(request, pk):
-    post = Cat.objects.get(pk=pk)
-    if request.method == 'PUT' or request.method == 'PATCH':
-        serializer = CatSerializer(post, data=request.data, partial=True)
+class APICatDetail(APIView):
+    def get(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        post.delete()
+
+    def patch(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def delete(self, request, pk):
+        cat = Cat.objects.get(pk=pk)
+        cat.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    serializer = CatSerializer(post)
-    return Response(serializer.data, status=status.HTTP_200_OK)
--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from cats.views import cat_detail, cat_list
+from cats.views import APICat, APICatDetail
 
 urlpatterns = [
-    path('cats/', cat_list),
-    path('cats/<int:pk>/', cat_detail),
+    path('cats/', APICat.as_view()),
+    path('cats/<int:pk>/', APICatDetail.as_view()),
 ]
